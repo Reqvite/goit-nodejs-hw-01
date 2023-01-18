7d4f2268f4f1d4859a874ae15d4873588c17e3e5
Reqvite <reqvite2233@gmail.com>
2023-01-18 22:25:43 +0200

    finish task

---
+++ README.md
@@ -0,0 +1,15 @@
+# Get and display the entire list of contacts in the form of a table (console.table)
+
+https://prnt.sc/5Da0gGrqd-ms
+
+# Get contact by id
+
+https://prnt.sc/tHy3l4H0v8FJ
+
+# Add a contact
+
+https://prnt.sc/5sPmLOSCxjdM
+
+# Delete contact
+
+https://prnt.sc/hUYCqNLIymrs
---
+++ contacts.js
@@ -0,0 +1,72 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const contactsPath = path.resolve("./db/contacts.json");
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parseData = JSON.parse(data);
+    console.table(parseData);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parseData = JSON.parse(data);
+    const searchedContact = parseData.find(({ id }) => id === contactId);
+    if (searchedContact === undefined) {
+      throw new Error("No contact with this ID");
+    }
+    console.table(parseData.find(({ id }) => id === contactId));
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parseData = JSON.parse(data);
+    const filterData = parseData.filter(({ id }) => id !== contactId);
+    if (filterData.length === parseData.length) {
+      throw new Error("No contact with this ID");
+    }
+    await fs.writeFile(contactsPath, JSON.stringify(filterData));
+    const updatedFile = await fs.readFile(contactsPath);
+    const parseUpdatedFile = JSON.parse(updatedFile);
+    console.table(parseUpdatedFile);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const addContact = async (name, email, phone) => {
+  const newContact = {
+    id: uuidv4(),
+    name,
+    email,
+    phone,
+  };
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parseData = JSON.parse(data);
+    const newData = [...parseData, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(newData));
+    const updatedFile = await fs.readFile(contactsPath);
+    const parseUpdatedFile = JSON.parse(updatedFile);
+    console.table(parseUpdatedFile);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
